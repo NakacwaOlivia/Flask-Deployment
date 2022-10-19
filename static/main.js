488074c598811135559b644409a6878be82f98a5
bruceMug <102650525+bruceMug@users.noreply.github.com>
2022-10-19 21:11:42 +0300

    added upload preview

--- static/main.js
+++ static/main.js
@@ -1,21 +1,4 @@
-$(document).ready(function() {
-    var brand = document.getElementById('image-id');
-    brand.className = 'attachment_upload';
-    brand.onchange = function() {
-        document.getElementById('fakeUploadLogo').value = this.value.substring(12);
-    };
-
-        function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            
-            reader.onload = function(e) {
-                $('.img-preview').attr('src', e.target.result);
-            };
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-    $("#image-id").change(function() {
-        readURL(this);
-    });
-});
+var loadFile = function(event) {
+    var image = document.getElementById('display_image');
+    image.src = URL.createObjectURL(event.target.files[0]);
+};
